8e34b7933a76a8713822d4ec897245e8f524b6ae
Yishuai Li <yishuai@cis.upenn.edu>
2018-06-06 01:28:52 +0000

    r100

--- vfa-current/Preface.v
+++ vfa-current/Preface.v
@@ -69,7 +69,7 @@
 
 (** Coq runs on Windows, Linux, and OS X.  The Preface of Volume 1
     describes the Coq installation you will need.  This edition was
-    built with Coq 8.7.1.
+    built with Coq 8.8.0.
 
     In addition, two of the chapters ask you to compile and run an
     OCaml program; having OCaml installed on your computer is helpful,
